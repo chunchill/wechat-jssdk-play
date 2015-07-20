957749cbbe8a4b7c8e6c65e4c9542adf703fa138
admin <admin>
2015-07-20 23:41:54 +0800

    add the upload API

--- wechat-jssdk-play/WeChatAppServices/Controllers/ImageController.cs
+++ wechat-jssdk-play/WeChatAppServices/Controllers/ImageController.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace WeChatAppServices.Controllers
 {
     public class ImageController : ApiController
     {
+        [HttpPost]
+        public void UploadFile()
+        {
+            if (HttpContext.Current.Request.Files.AllKeys.Any())
+            {
+                // Get the uploaded image from the Files collection
+                var httpPostedFile = HttpContext.Current.Request.Files["UploadedImage"];
+                if (httpPostedFile != null)
+                {
+                    // Validate the uploaded image(optional)
+
+                    // Get the complete file path
+                    var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/UploadedFiles"), httpPostedFile.FileName);
+
+                    // Save the uploaded file to "UploadedFiles" folder
+                    httpPostedFile.SaveAs(fileSavePath);
+                }
+            }
+        }
     }
 }
