5fb126b0aacdba7ab4e44d9fb54ab10640d31dfa
admin <admin>
2015-07-20 22:49:37 +0800

    redesign the structure

---
+++ wechat-jssdk-play/WeChatAppServices/Controllers/WechatAppController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WeChatAppServices.Models;
+
+namespace WeChatAppServices.Controllers
+{
+    public class WechatAppController : ApiController
+    {
+        private SQLLiteContext db = new SQLLiteContext();
+
+        [HttpGet]
+        public HttpResponseMessage Signature(string jsTicket, string url)
+        {
+            var message = new HttpResponseMessage();
+            
+            var app = new WeChatApp();
+            app.AppID = "123";
+            app.Token = "abcde";
+            if (!db.WebChatApps.Any(item => item.AppID == "123"))
+            {
+                db.WebChatApps.Add(app);
+                db.SaveChanges();
+            }
+            message.Content = new StringContent(string.Format("jsTicket:{0},url:{1}", jsTicket, url));
+            message.StatusCode = HttpStatusCode.OK;
+            return message;
+        }
+    }
+}
---
+++ wechat-jssdk-play/WeChatAppServices/Models/SQLLiteContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WeChatAppServices.Models
+{
+    public class SQLLiteContext : DbContext
+    {
+        public SQLLiteContext()
+            : base("SqliteDbConn")
+        {
+
+        }
+        public DbSet<WeChatApp> WebChatApps { get; set; }
+    }
+
+
+    public class WeChatApp
+    {
+        public int ID { get; set; }
+        public string AppID { get; set; }
+        public string Token { get; set; }
+    }
+
+    public class UploadedImages
+    {
+        public int ID { get; set; }
+        public int OpenID { get; set; }
+        public string ImageServerPath { get; set; }
+        public DateTime UploadDateTime { get; set; }
+    }
+}
